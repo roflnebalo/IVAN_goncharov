f66335b923d7e35f8f41f88ee6885d427fb5ab4e
gon4a <gon4a@DESKTOP-3U186VD>
2024-09-05 20:04:38 +0300

    Добавьте файлы проекта.

---
+++ Project1/project1.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <locale.h>
+void main()
+{
+	puts("                  *                                ");
+	puts("                 *_*  /\\                          ");
+	puts("                 | | /  \\                         ");
+	puts("                 | |/ __ \\                        ");
+	puts("                 | / |  | \\                       ");
+	puts("                 |/  |__|  \\                      ");
+	puts("                 /__________\\                     ");
+	puts("                 |          |                      ");
+	puts("                 |          |                      ");
+	puts("                 |  ___     |                      ");
+	puts("                 | |_|_|  __|                      ");
+	puts("                 | |_|_| |  |                      ");
+	puts("                 |       |* |                      ");
+	puts("                 |_______|__|                      ");
+}
